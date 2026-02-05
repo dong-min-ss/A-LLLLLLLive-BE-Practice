85186c0606a1e141fd3143368dd86730457ca435
Jimin <gift040404@naver.com>
2026-02-05 14:35:43 +0900

    feat: 스프링부트 초기설정

---
+++ aL7ive/src/main/java/com/aL7ive/aL7ive/AL7iveApplication.java
@@ -0,0 +1,13 @@
+package com.aL7ive.aL7ive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AL7iveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AL7iveApplication.class, args);
+	}
+
+}
---
+++ aL7ive/src/test/java/com/aL7ive/aL7ive/AL7iveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aL7ive.aL7ive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AL7iveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
